5882b00b0742af81f29d577c0156f72fc1e9c094
Wei Huan Chen <wei.chen@vtex.com.br>
2022-04-27 13:23:21 -0400

    3d packing algo

--- dotnet/Controllers/RoutesController.cs
+++ dotnet/Controllers/RoutesController.cs
@@ -1,11 +1,16 @@
 ﻿namespace service.Controllers
 {
     using System;
+    using System.Collections.Generic;
     using System.Diagnostics;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Http;
     using Microsoft.AspNetCore.Mvc;
     using Vtex.Api.Context;
+    using Newtonsoft.Json;
+    using CromulentBisgetti.ContainerPacking;
+    using CromulentBisgetti.ContainerPacking.Entities;
+    using CromulentBisgetti.ContainerPacking.Algorithms;
 
     public class RoutesController : Controller
     {
@@ -17,6 +22,22 @@
         public string test()
         {
             return "string";
+        }
+
+        public string pack()
+        {
+            List<Container> containers = new List<Container>();
+            containers.Add(new Container(1, 2, 2, 3));
+
+            List<Item> itemsToPack = new List<Item>();
+            itemsToPack.Add(new Item(1, 2, 2, 2, 2));
+
+            List<int> algorithms = new List<int>();
+            algorithms.Add((int)AlgorithmType.EB_AFIT);
+
+            List<ContainerPackingResult> result = PackingService.Pack(containers, itemsToPack, algorithms);
+            Console.WriteLine(JsonConvert.SerializeObject(result));
+            return JsonConvert.SerializeObject(result);
         }
     }
 }
